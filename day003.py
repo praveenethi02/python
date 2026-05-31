2676f757b590b9318277c655e5aa01788fb6a508
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-01 01:59:03 +0530

    edit the day 3

--- day003.py
+++ day003.py
@@ -1,12 +1,19 @@
 #first chance
 
-'''print("welcome the roller coster")
+print("welcome the roller coster")
 height = int(input("Enter your height in cm ? "))
 
 if(height>=120):
     print("You can ride")
+    age = int(input("Enter age : "))
+    if(age<=12):
+        print("pay $5")
+    elif(age<=18):
+        print("pay $6")
+    else:
+        print("pay $8")
 else:
-    print("you can not ride")'''
+    print("you can not ride")
 
 #even or odd
 '''number = int(input("Enter the number :"))
