11a09039269e5e3e7a00211ca74b97161cc5c75d
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-01 23:30:58 +0530

    Random module 1

---
+++ day004.py
@@ -0,0 +1,24 @@
+import random
+
+#random integer and random float
+'''random_integer = random.randint(1, 10)
+print(random_integer)
+
+random_float = random.random() * 5
+print(random_float)
+
+love = random.randint(1, 100)
+print(f"your love score is {love}")'''
+
+#head or tail
+
+chance = random.randint(0,1)
+if(chance==1):
+    print("Head")
+else:
+    print("Tail")
+  
+
+
+
+
