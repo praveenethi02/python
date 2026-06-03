351f8aa0f93f3db5c72c522c9f4f3bf7420d2906
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-04 03:27:17 +0530

    PizzBuzz game and Random password genarator

--- day005.py
+++ day005.py
@@ -1,3 +1,5 @@
+import random
+
 '''fruits = ["apple", "banana", "cherry", "date", "elderberry"]
 
 for fruit in fruits:
@@ -67,13 +69,81 @@ for number in range(1,no):
 print(total)'''
 
 #2nd method
-num = int(input("Enter number : "))
+'''num = int(input("Enter number : "))
 no = num + 1
 total = 0
 for num in range(0,no,2):
     total+=num
 
-print(total)
+print(total)'''
+
+#PizzBuzz game 
+
+'''num = int(input("Enter the number : "))
+no = num + 1
+
+for number in range(1,no):
+    if(number%3==0 and number%5==0):
+        print("PizzBuzz")
+    elif(number%3==0):
+        print("Pizz")
+    elif(number%5==0):
+        print("Buzz")
+    else:
+        print(number)'''
+
+#Password Genarator (Day final project)
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
+           'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 
+           'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 
+           'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 
+           'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n")) 
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+'''password = ""
+
+#Easy method  (chjj(*&143))
+
+for char in range(1,nr_letters+1):
+    rand_char = random.choice(letters)
+    password = password + rand_char
+
+for char in range(1,nr_symbols+1):
+    rand_sym = random.choice(symbols)
+    password = password + rand_sym
+
+for char in range(1,nr_numbers+1):
+    rand_num = random.choice(numbers)
+    password = password + rand_num
+
+print(f"Password : {password}")'''
+
+#Hard Metod  
+password_list = []
 
 
+for char in range(1,nr_letters+1):
+   password_list.append(random.choice(letters))
 
+for char in range(1,nr_symbols+1):
+    password_list += (random.choice(symbols))
+    
+for char in range(1,nr_numbers+1):
+   password_list += (random.choice(numbers))
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+pw = ""
+
+for char in password_list:  #list to varible
+   pw += char
+
+print(f"Password is : {pw}")
