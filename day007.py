8e911b2b2137df594d6d26b17c342665f8ec8f75
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-06 18:04:52 +0530

    day 07

---
+++ day007.py
@@ -0,0 +1,14 @@
+word_list = ["ardvark", "baboon", "camel"]
+
+import random
+chosen_word = random.choice(word_list)
+
+
+guess = input("Guess a letter: ").lower()
+
+for letter in chosen_word:
+    if letter == guess:
+        print("correct")
+    
+    else:
+        print("wrong")
