356799362ea9c17695ea2f853c0b08c50fb5419d
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-06 17:42:34 +0530

    day 06 last

--- day006.py
+++ day006.py
@@ -1,7 +1,28 @@
 #Defined a new function called my_function
 
+'''def my_function():
+    print("Hello world")
+    print("this is type of defined function")
+
+my_function()'''
+
+#functin using for loop in python
+
+'''def my_function():
+    print("Hello world")
+    print("this is type of defined function")
+
+for step in range (6):
+    my_function()'''
+
+#while loop in python
+
 def my_function():
     print("Hello world")
     print("this is type of defined function")
 
-my_function()
+hurdles = 6
+while hurdles > 0 :
+    my_function()
+    hurdles -= 1
+    print(hurdles)
