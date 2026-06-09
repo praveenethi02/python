30348197d9a397cb31355338a03c18fb89de9758
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-09 23:39:02 +0530

    add the hangman pictures

--- day007.py
+++ day007.py
@@ -1,7 +1,71 @@
-word_list = ["ardvark", "baboon", "camel"]
 
 import random
-chosen_word = random.choice(word_list)
+#Hangman game stages
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+#word_list = ["ardvark", "baboon", "camel"]
+
+import hangman_words
+chosen_word = random.choice(hangman_words.word_list)
+word_length = len(chosen_word)
+end_of_game = False
+
+lives = 6 
 
 Display = []
 
@@ -10,19 +74,26 @@ for _ in range(len(chosen_word)):
 print(Display)
 
 
-end_of_game = False
-
 while not end_of_game:  
     guess = input("Guess a letter: ").lower()
 
     for position in range(len(chosen_word)):
         letter = chosen_word[position]
+        #print(f"Current position: {position}\n Current letter: {letter}\n Guessed letter: {guess}")
         if letter == guess:
             Display[position]= letter
-        
+
+    if guess not in chosen_word:
+        lives -= 1
+        if lives == 0: 
+            end_of_game = True
+            print("You lose.")
 
     print(f"Word is : {Display}")
 
     if "_" not in Display:
         end_of_game = True
         print("You win.")
+
+
+    print(stages[lives])
