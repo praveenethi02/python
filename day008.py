232981de5d704b1ac9a6ecba51b559a10a95bb0d
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-19 10:28:48 +0530

    Day 008 1st step - about functions

---
+++ day008.py
@@ -0,0 +1,29 @@
+'''def greet():
+    print("This is new function")
+    print("hello praveen")
+    print("what is your daily shedule")
+
+greet()'''
+
+
+'''def greet_with_name(name):
+    print(f"This is new function {name}")
+    print(f"hello {name}")
+    print(f"what is your daily shedule {name}")
+
+greet_with_name("praveen")'''
+
+
+'''def greet_with(name, location):
+    print(f"This is new function {name}")
+    print(f"hello {name}")
+    print(f"what is your daily shedule in {location}")
+
+greet_with("praveen", "matale")'''
+
+def greet_with(name, location):
+    print(f"This is new function {name}")
+    print(f"hello {name}")
+    print(f"what is your daily shedule in {location}")
+
+greet_with(location = "matale", name = "praveen")
