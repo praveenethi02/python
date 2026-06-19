d2c0e7bc4d2810a42314c28ab6ca97aec663b529
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-19 18:04:24 +0530

    Day 008 activity  01

--- day008.py
+++ day008.py
@@ -21,9 +21,24 @@ greet_with_name("praveen")'''
 
 greet_with("praveen", "matale")'''
 
-def greet_with(name, location):
+'''def greet_with(name, location):
     print(f"This is new function {name}")
     print(f"hello {name}")
     print(f"what is your daily shedule in {location}")
 
-greet_with(location = "matale", name = "praveen")
+greet_with(location = "matale", name = "praveen")'''
+
+
+#Activity 1
+
+import math
+
+def paint_cal(height, width, cover):
+    cans = (height*width)/cover
+    round_up_cans = math.ceil(cans)
+    print(f"You'll need {round_up_cans} cans of paint.")
+
+test_h = int(input("Height : "))
+test_w = int(input("Width : "))
+coverage = 5
+paint_cal(height=test_h, width=test_w, cover=coverage)
