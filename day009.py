04892380be78fa6300877af486113d61bc91eb44
Praveen Thilanka <praveenethipola@gmail.com>
2026-06-22 19:24:21 +0530

    Day 09 part 2

--- day009.py
+++ day009.py
@@ -18,11 +18,35 @@ print(no_item)
 #edit item in a dictionary
 
 progrmming_dictionary["Bug"] = "Edit the previous one"
-print(progrmming_dictionary["Bug"])'''
+print(progrmming_dictionary["Bug"])
 
 #Loop through a dictionary
 
 for key in progrmming_dictionary:
     print(key)
-    print(progrmming_dictionary[key])
+    print(progrmming_dictionary[key])'''
+
+#Exercise 01
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+student_grades = {}
+
+for marks in student_scores:
+    score = student_scores[marks]
+    if score >= 75:
+        student_grades[marks] = "A"
+    elif score > 50:
+        student_grades[marks] = "B"
+    elif score > 25:
+        student_grades[marks] = "C"
+    else:
+        student_grades[marks] = "Fail"
+
+print(student_grades)
 
