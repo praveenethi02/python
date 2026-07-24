452ac93e7b5439ca129ebd6024e3da35960f8ada
Praveen Thilanka <praveenethipola@gmail.com>
2026-07-24 23:48:17 +0530

    day 012 part 02

--- day012.py
+++ day012.py
@@ -1,9 +1,47 @@
-#scope
+####################scope
+
+# enemies = 1 
+# def increase_enemies():
+#     enemies = 2
+#     print(f"enemise inside function: {enemies}")
+
+# increase_enemies()
+# print(f"enemise outside function: {enemies}")
+
+#################Local scope
+
+# def drink():
+#     potion = 2
+#     print(potion)
+
+# drink()
+
+################Global scope
+# play = 10
+
+# def dd():
+#     poe = 2
+#     print(play)
+
+# dd()
+
+#############################
+# game_level = 3
+# def create_enemy():
+#     enemies = ["Skelton","Zombie","Alien"]
+#     if game_level < 5:
+#         new_enemy = enemies[0]
+
+#     print(new_enemy)
+
 
 enemies = 1 
-def increase_enemies():
-    enemies = 2
-    print(f"enemise inside function: {enemies}")
 
-increase_enemies()
+def increase_enemies():
+    global enemies 
+    # enemies += 1
+    # print(f"enemise inside function: {enemies}")
+    return enemies + 1
+
+enemies = increase_enemies()
 print(f"enemise outside function: {enemies}")
